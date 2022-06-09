9aa7550844ce72aad867101dbbe688de0c38f9cd
Nataliia Lyfar <lifarnatala@gmail.com>
2022-06-09 19:32:27 +0300

    storage & pagination

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import './js/fetch/trendingMovie.js';
 import './js/pagination.js';
 import './js/scrollTop';
 import './js/footer-modal.js';
-import './js/fetch/trendingMovie.js';
+
 import './js/fetch/fetchByKey.js';
