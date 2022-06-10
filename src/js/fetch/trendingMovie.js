0df5f92d1fe0c80e5534cee9c602d99318df2f10
Artem <talibanssevik@gmail.com>
2022-06-10 23:10:03 +0300

    add loader js

--- src/js/fetch/trendingMovie.js
+++ src/js/fetch/trendingMovie.js
@@ -2,6 +2,7 @@ import { currentPage, defineResultsPerPage, pageRefs } from '../pagination.js';
 import axios from 'axios';
 import { BASE_URL, API_KEY } from '../api/api';
 import movieCard from '../../template/movieCard.hbs';
+import { showLoader, hideLoader } from '../loader.js';
 const axios = require('axios');
 // const refsMovie = {
 //   page: currentPage,
@@ -15,22 +16,25 @@ const gallery = document.querySelector('.gallery');
 const renderMovie = data =>
   gallery.insertAdjacentHTML('beforeend', movieCard(data));
 
-export const fetchPopularMovie = async page => 
-         await axios.get(
-          `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${currentPage}`
-        ).catch((e)=>console.error(e))
-         
-    // const allGenres = getGenres();
-    // const fullTrendData = dataCombine(movies, allGenres);
- 
-export const requestForPage = async() => {
-  await fetchPopularMovie(currentPage).then(({data}) => {
-console.log(data);
-const movies = data.results;
-const totalPages = data.total_pages;
-pageRefs.lastPageBtn.textContent = totalPages;
-console.log(totalPages);
-renderMovie(movies);
+export const fetchPopularMovie = async page =>
+  await axios.get(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${currentPage}`
+
+  ).catch((e) => console.error(e))
+
+// const allGenres = getGenres();
+// const fullTrendData = dataCombine(movies, allGenres);
+
+export const requestForPage = async () => {
+  hideLoader();
+  await fetchPopularMovie(currentPage).then(({ data }) => {
+    console.log(data);
+    const movies = data.results;
+    const totalPages = data.total_pages;
+    pageRefs.lastPageBtn.textContent = totalPages;
+    console.log(totalPages);
+    renderMovie(movies);
+    showLoader();
   });
 }
 requestForPage();
