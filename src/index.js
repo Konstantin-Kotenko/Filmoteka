f2b5b647eac6630a7e8fdb16fa27bc2866b64f23
Denis <denis.tolmachev@gmail.com>
2022-06-11 22:20:50 +0300

    correct code

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import './js/storage-theme-switch';
 import './js/modal_movie.js';
 import './js/storage.js';
 import './js/fetch/fetchTrailer';
-
+import './js/watched.js';
--- src/js/watched.js
+++ src/js/watched.js
@@ -1,16 +1,6 @@
 import { getFromStorage } from './storage';
-
-// const libraryContainer = document.querySelector('.gallery--library')
-// const watchedBtn = document.querySelector(".btn--watched");
-
-// const watchedListRender = () => {
-//     const watchedList = getFromStorage('watchedList')
-//     watchedList.map(id => {
-//         libraryContainer.insertAdjacentHTML('beforeend', movieCard(id))
-//     })
-// }
-
-// watchedBtn.addEventListener("click", watchedListRender);
+import { movieCard } from '../template/movieCard.hbs';
+import { fetchMovie } from './modal_movie';
 
 const btnWatched = document.querySelector('.btn--watched');
 const libraryGallery = document.querySelector('.gallery--library');
@@ -20,10 +10,8 @@ const clearLibrary = () => (libraryGallery.innerHTML = '');
 const showFilms = key => {
   clearLibrary();
 
-  //const localStr = getFromStorage(key);
-
-  const watchedArr = getFromStorage('filmsWatched');
-  watchedArr.forEach(id => searchMovieById.then(data => renderMovie(data)));
+  const watchedArr = getFromStorage(key);
+  watchedArr.forEach(id => fetchMovie(id).then(data => renderMovie(data)));
 };
 
 const watchedBtn = () => {
