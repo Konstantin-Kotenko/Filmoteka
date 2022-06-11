2c2b11dbf94e658385c325da8ad2afe1615f3741
Denis <denis.tolmachev@gmail.com>
2022-06-11 23:13:02 +0300

    correct code

--- src/js/watched.js
+++ src/js/watched.js
@@ -3,24 +3,43 @@ import { movieCard } from '../template/movieCard.hbs';
 import { fetchMovie } from './modal_movie';
 import { renderMovie } from './fetch/fetchByKey';
 
-const btnWatched = document.querySelector('.btn--watched');
+// const btnWatched = document.querySelector('.btn--watched');
+// const libraryGallery = document.querySelector('.gallery--library');
+
+// const clearLibrary = () => (libraryGallery.innerHTML = '');
+
+// const showFilms = key => {
+//   clearLibrary();
+
+//   const watchedArr = getFromStorage(key);
+//   watchedArr.forEach(id => fetchMovie(id).then(data => renderMovie(data)));
+// };
+
+// const watchedBtn = () => {
+//   refs.watchedBtnRef.classList.add('orange');
+//   refs.queueBtnRef.classList.remove('orange');
+// };
+
+// btnWatched.addEventListener('click', () => {
+//   showFilms('filmsWatched');
+//   watchedBtn();
+// });
+
+const watchedBtn = document.querySelector('.btn--watched');
 const libraryGallery = document.querySelector('.gallery--library');
+console.log(watchedBtn);
+console.log(libraryGallery);
 
-const clearLibrary = () => (libraryGallery.innerHTML = '');
+const watchedArr = getFromStorage('filmsWatched');
+console.log(watchedArr);
 
-const showFilms = key => {
-  clearLibrary();
-
-  const watchedArr = getFromStorage(key);
-  watchedArr.forEach(id => fetchMovie(id).then(data => renderMovie(data)));
+const showFilms = () => {
+  watchedArr.forEach(id =>
+    fetchMovie(id).then(data =>
+      libraryGallery.insertAdjacentHTML('beforeend', renderMovie(data))
+    )
+  );
 };
+console.log(showFilms);
 
-const watchedBtn = () => {
-  refs.watchedBtnRef.classList.add('orange');
-  refs.queueBtnRef.classList.remove('orange');
-};
-
-btnWatched.addEventListener('click', () => {
-  showFilms('filmsWatched');
-  watchedBtn();
-});
+watchedBtn.addEventListener('click', showFilms);
