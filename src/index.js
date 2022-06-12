42622a351718bd11edbb4a4bf7eebc422f99d642
Larin Artem <larin_art@ukr.net>
2022-06-12 10:36:04 +0300

    finish fix theme btn

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import './js/footer-modal.js';
 import './js/fetch/fetchByKey.js';
 import './js/fetch/fetchDateAndGenres.js';
 import './js/change-theme';
-import './js/storage-theme-switch';
 import './js/modal_movie.js';
 import './js/storage.js';
