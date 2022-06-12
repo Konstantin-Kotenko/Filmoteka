945a8bb9d7c62b76da223efbc4ecf6d021552c31
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-06-12 18:12:30 +0300

    fix-loginForm

--- src/js/loginForm.js
+++ src/js/loginForm.js
@@ -3,11 +3,13 @@ const loginForm = document.querySelector('form.login');
 const loginBtn = document.querySelector('label.login');
 const signupBtn = document.querySelector('label.signup');
 const signupLink = document.querySelector('form .signup-link a');
+const iconBack = document.querySelector('#iconBack');
 
 const onHandleSignupBtn = () => {
   loginForm.style.marginLeft = '-50%';
   loginText.style.marginLeft = '-50%';
 };
+
 const onHandleloginBtn = () => {
   loginForm.style.marginLeft = '0%';
   loginText.style.marginLeft = '0%';
@@ -18,6 +20,11 @@ const onHandleSignupLink = () => {
   return false;
 };
 
+const onHandleIconBack = () => {
+  window.location.replace('index.html');
+};
+
+iconBack?.addEventListener('click', onHandleIconBack);
 signupBtn?.addEventListener('click', onHandleSignupBtn);
 loginBtn?.addEventListener('click', onHandleloginBtn);
 signupLink?.addEventListener('click', onHandleSignupLink);
