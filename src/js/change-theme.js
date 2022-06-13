eb78f54257579206c0f094ced3d1a56ffe3077bd
Larin Artem <larin_art@ukr.net>
2022-06-13 18:58:36 +0300

    fix theme

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -3,26 +3,26 @@ const toggle = document.querySelector('.theme-switch__toggle');
 const footerDarktheme = document.querySelector('.footer');
 
 const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
 };
 
 body.classList.add(Theme.LIGHT);
 
 if (localStorage.getItem('theme')) {
-  toggle.setAttribute('checked', true);
-  body.classList.add(Theme.DARK);
-  footerDarktheme.classList.add(Theme.DARK);
+    toggle.setAttribute('checked', true);
+    body.classList.add(Theme.DARK);
+    footerDarktheme?.classList.add(Theme.DARK);
 }
 
 const onChange = () => {
-  body.classList.toggle(Theme.DARK);
-  footerDarktheme.classList.toggle(Theme.DARK);
-  if (body.classList.contains(Theme.DARK)) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.removeItem('theme');
-  }
+    body.classList.toggle(Theme.DARK);
+    footerDarktheme?.classList.toggle(Theme.DARK);
+    if (body.classList.contains(Theme.DARK)) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.removeItem('theme');
+    }
 };
 
 toggle?.addEventListener('change', onChange);
