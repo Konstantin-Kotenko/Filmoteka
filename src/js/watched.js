0a009b5c64d434c3220ae6e5617e19c51ad15f96
Denis <denis.tolmachev@gmail.com>
2022-06-15 00:05:37 +0300

    correct errors in localstorage js

--- src/js/watched.js
+++ src/js/watched.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 import { getFromStorage } from './storage';
 import oneMovieCard from '../template/oneMoviecard.hbs';
 import { BASE_URL, API_KEY } from './api/api';
-import {renderingPaginationMarkup} from './pagination.js'
+import { renderingPaginationMarkup } from './pagination.js';
 
 const watchedBtn = document.querySelector('.btn--watched');
 const libraryGallery = document.querySelector('.gallery--library');
-
 
 // let libraryPage = 1;
 // let totalPages = 1;
@@ -26,24 +25,22 @@ const fetchById = async id => {
 function slicins(string) {
   return string.slice(0, 4);
 }
-// Does not work the pagination of librarry page. 
+// Does not work the pagination of librarry page.
 // It sets the pagination from the request function of trendongMovie
 
 export const requestForWatched = async () => {
- 
+  libraryGallery.innerHTML = '';
   const watchedArr = getFromStorage('filmsWatched');
   const arrayForRender = watchedArr.map(id => {
     fetchById(id).then(result => {
       const { data } = result;
       data.release_date = slicins(data.release_date);
       libraryGallery?.insertAdjacentHTML('beforeend', oneMovieCard(data));
-      
+
       console.log(data);
-    
     });
-    
   });
-  // renderingPaginationMarkup(libraryPage, totalPages);
+  //renderingPaginationMarkup(libraryPage, totalPages);
 };
 
 requestForWatched();
