bbf192c26de68009b865014a96f60ab70992bc32
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-06-16 12:48:37 +0300

    fixWatchedAndQueue

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,34 +1,35 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { getFromStorage } from './storage';
 import oneMovieCard from '../template/oneMoviecard.hbs';
-import { BASE_URL, API_KEY } from './api/api';
-import {refs} from './refs.js'
+// import { BASE_URL, API_KEY } from './api/api';
+import { refs } from './refs.js';
+import { getDataFilms } from '../api/getDataFilms';
 
-const {libraryGallery, btnWatched, btnQueue} = refs.library;
+const { libraryGallery, btnWatched, btnQueue } = refs.library;
 
-const fetchById = async id => {
-  try {
-    const customIdAxios = axios.create({
-      baseURL: `${BASE_URL}/movie/${id}?api_key=${API_KEY}`,
-    });
-    const data = await customIdAxios.get('');
-    return data;
-  } catch {
-    Notiflix.Notify.failure('Search result not successful');
-  }
-};
-const dataCombine = (movie) => {
+// const fetchById = async id => {
+//   try {
+//     const customIdAxios = axios.create({
+//       baseURL: `${BASE_URL}/movie/${id}?api_key=${API_KEY}`,
+//     });
+//     const data = await customIdAxios.get('');
+//     return data;
+//   } catch {
+//     Notiflix.Notify.failure('Search result not successful');
+//   }
+// };
+const dataCombine = movie => {
   return {
     ...movie,
     year: movie.release_date.slice(0, 4),
+  };
 };
-}
 export const requestForQueue = async () => {
   libraryGallery.innerHTML = '';
   const queuedArr = getFromStorage('filmsQueue');
   const arrayRender = queuedArr.map(id => {
-    fetchById(id).then(result => {
-      const { data } = result;
+    getDataFilms(id).then(result => {
+      const data = result;
       const fullData = dataCombine(data);
       libraryGallery?.insertAdjacentHTML('beforeend', oneMovieCard(fullData));
     });
--- src/js/watched.js
+++ src/js/watched.js
@@ -1,40 +1,26 @@
-import axios from 'axios';
 import { getFromStorage } from './storage';
 import oneMovieCard from '../template/oneMoviecard.hbs';
-import { BASE_URL, API_KEY } from './api/api';
-import {refs} from './refs.js';
+import { refs } from './refs.js';
 import { dynamicRefs } from './dynamicRefs';
 import { showMovieCard } from './modal_movie';
+import { getDataFilms } from '../api/getDataFilms';
 
-const {libraryGallery, btnWatched, btnQueue} = refs.library;
-console.log(refs.library);
-const fetchById = async id => {
-  try {
-    const customIdAxios = axios.create({
-      baseURL: `${BASE_URL}/movie/${id}?api_key=${API_KEY}`,
-    });
-    const data = await customIdAxios.get('');
-    return data;
-  } catch {
-    Notiflix.Notify.failure('Search result not successful');
-  }
-};
+const { libraryGallery, btnWatched, btnQueue } = refs.library;
 
-
-const dataCombine = (movie) => {
+const dataCombine = movie => {
   return {
     ...movie,
     year: movie.release_date.slice(0, 4),
+  };
 };
-}
 export const requestForWatched = async () => {
   const liveRefs = dynamicRefs();
   libraryGallery.innerHTML = '';
   const watchedArr = getFromStorage('filmsWatched');
   const arrayForRender = watchedArr.map(id => {
-    fetchById(id).then(result => {
-      const { data } = result;
-   const fullData = dataCombine(data);
+    getDataFilms(id).then(result => {
+      const data = result;
+      const fullData = dataCombine(data);
       libraryGallery?.insertAdjacentHTML('beforeend', oneMovieCard(fullData));
     });
   });
